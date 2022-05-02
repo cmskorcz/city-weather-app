14277592474f78fc7e79c2d5ea3376839e101e20
cmskorcz <chaseskorey@icloud.com>
2022-05-02 18:25:11 -0400

    name of city displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 const GEO_API_KEY = '1f6d1a1e5f8971c7d7609741b9cbc241'
+const WEATHER_API_KEY = 'bc8dee79f60c665335a895eb0f9a0afb'
 
 let searchHistoryEl = document.getElementById('search-history');
 let searchInputEl = document.getElementById('search-input');
 let searchFormEl = document.getElementById('search-form');
-
+let weatherContainerEl = document.getElementById('today-weather');
 
 
 const getCityGeo = (city) => {
@@ -14,16 +15,45 @@ const getCityGeo = (city) => {
             if(res.ok) {
                 res.json()
                 .then((data) => {
-                    getCityWeather(data.data[0].latitude, data.data[0].longitude);
+                    getCityWeather(data.data[0].latitude, data.data[0].longitude, data.data[0].name);
                 })
             } else {
-                alert('There was an error with your search')
+                alert('There was an error with your search');
             }
         })
         .catch((err) => {
             alert('There was an error with your search');
             console.log(err);
-        })
+        });
+};
+
+const getCityWeather = (lat, long, name) => {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}&units=imperial`
+
+    fetch(apiUrl)
+        .then((res) => {
+            if(res.ok) {
+                res.json()
+                    .then((data) => {
+                        displayWeather(data, name)
+                });
+            } else {
+                alert('There was an error with finding the weather');
+            }
+        });
+};
+
+const displayWeather = (data, name) => {
+    let temp = data.current.temp
+    let wind = data.current.wind_speed
+    let humidity = data.current.humidity
+    let uv = data.current.uvi
+    let city = name
+
+    let cityNameEl = document.getElementById('city-name');
+    cityNameEl.textContent = city;
+
+
 }
 
 const formSubmitHandler = (event) => {
