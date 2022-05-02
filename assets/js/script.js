a741f1a46ea856fd3933f85f5ec48d7b08d6d28e
cmskorcz <chaseskorey@icloud.com>
2022-05-02 12:34:51 -0400

    search input converted to lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
+const GEO_API_KEY = '1f6d1a1e5f8971c7d7609741b9cbc241'
+
 let searchHistoryEl = document.getElementById('search-history');
 let searchInputEl = document.getElementById('search-input');
 let searchFormEl = document.getElementById('search-form');
 
-const getCityWeather = (city) => {
-    // let apiUrl = 
+
+
+const getCityGeo = (city) => {
+    let apiURL = `http://api.positionstack.com/v1/forward?access_key=${GEO_API_KEY}&query=${city}`;
+
+    fetch(apiURL)
+        .then((res) => {
+            if(res.ok) {
+                res.json()
+                .then((data) => {
+                    getCityWeather(data.data[0].latitude, data.data[0].longitude);
+                })
+            } else {
+                alert('There was an error with your search')
+            }
+        })
+        .catch((err) => {
+            alert('There was an error with your search');
+            console.log(err);
+        })
 }
 
 const formSubmitHandler = (event) => {
@@ -11,11 +31,11 @@ const formSubmitHandler = (event) => {
     let city = searchInputEl.value.trim();
 
     if (city) {
-        getCityWeather(city);
+        getCityGeo(city);
         searchInputEl.value = '';
     } else {
         alert('Please enter a city before submitting!');
     };
 };
 
-searchFormEl.addEventListener('submit', displaySearchHistory);
+searchFormEl.addEventListener('submit', formSubmitHandler);
