ed0466aa7b8bafd685cee69ebb5b11183d6bb9ac
Chase Skorczewski <chaseskorey@icloud.com>
2022-05-03 13:00:27 -0400

    todays forcast displays in container

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,22 @@ const WEATHER_API_KEY = 'bc8dee79f60c665335a895eb0f9a0afb'
 let searchHistoryEl = document.getElementById('search-history');
 let searchInputEl = document.getElementById('search-input');
 let searchFormEl = document.getElementById('search-form');
-let weatherContainerEl = document.getElementById('today-weather');
+let weatherContainerEl = document.getElementById('today-weather-container');
+let forcastListEl = document.getElementById('today-weather-list');
 
 
 const getCityGeo = (city) => {
-    let apiURL = `http://api.positionstack.com/v1/forward?access_key=${GEO_API_KEY}&query=${city}`;
+    let apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${WEATHER_API_KEY}`;
 
     fetch(apiURL)
         .then((res) => {
             if(res.ok) {
                 res.json()
                 .then((data) => {
-                    getCityWeather(data.data[0].latitude, data.data[0].longitude, data.data[0].name);
+                    let lat = data[0].lat;
+                    let lon = data[0].lon;
+                    let cityName = data[0].name
+                    getCityWeather(lat, lon, cityName);
                 })
             } else {
                 alert('There was an error with your search');
@@ -27,15 +31,15 @@ const getCityGeo = (city) => {
         });
 };
 
-const getCityWeather = (lat, long, name) => {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}&units=imperial`
+const getCityWeather = (lat, lon, cityName) => {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`
 
     fetch(apiUrl)
         .then((res) => {
             if(res.ok) {
                 res.json()
                     .then((data) => {
-                        displayWeather(data, name)
+                        displayWeather(data, cityName)
                 });
             } else {
                 alert('There was an error with finding the weather');
@@ -43,18 +47,31 @@ const getCityWeather = (lat, long, name) => {
         });
 };
 
-const displayWeather = (data, name) => {
-    let temp = data.current.temp
-    let wind = data.current.wind_speed
-    let humidity = data.current.humidity
+const displayWeather = (data, cityName) => {
+    let temp = `${data.current.temp} °F`;
+    let wind = `${data.current.wind_speed} MPH`;
+    let humidity = `${data.current.humidity}%`;
     let uv = data.current.uvi
-    let city = name
+
+    let forcastArray = [temp, wind, humidity, uv];
+
+    let city = cityName
 
     let cityNameEl = document.getElementById('city-name');
     cityNameEl.textContent = city;
 
+    displayForcast(forcastArray);
 
-}
+};
+
+const displayForcast = (arr) => {
+    arr.forEach((item) => {
+        let forcastListItemEl = document.createElement('li');
+        forcastListItemEl.className = 'list-group-item';
+        forcastListItemEl.textContent = item;
+        forcastListEl.appendChild(forcastListItemEl);
+    });
+};
 
 const formSubmitHandler = (event) => {
     event.preventDefault();
